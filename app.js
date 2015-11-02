c2c1e36f0c47c2135e68fa514e9adbf6f3b1ad35
Your Name <you@example.com>
2015-11-02 23:26:45 +0100

    Commit initial

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+// imports =====================================================================
+
+// Ces imports sont utiles pour le système de logging
+var config = require('./config.json'); // load the config
+
+// Évidemment on a besoin de express et de socket.io
+var express = require('express'); // load express
+var favicon = require('serve-favicon');
+var app = express(); // create express app
+var server = require('http').createServer(app); // create webserver
+
+app.use(favicon(__dirname + '/static/dist/favicon.png'));
+app.use(express.static(__dirname + '/static/dist'));
+
+
+
+require('./app')(app);
+
+server.listen(config.port, config.ip);
+console.log('App listening on ' + config.ip + ':' + config.port);
